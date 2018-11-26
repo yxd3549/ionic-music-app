54eb2f50afe963187230aa53763d096b9370cda9
yxd3549 <yxd3549@rit.edu>
2018-11-26 13:51:51 -0500

    Need to fix things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { Media } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, Media, File],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor( private file: File, private media: Media) { }
+
+  private song: MediaObject;
+
+  private isSongPaused = false;
+
+  public playMusic() {
+  console.log( 'playMusic called' );
+  console.log( 'path: ' + this.file.dataDirectory );
+  // if song MediaObject doesn't already exist, create a new one
+  try {
+    if ( !this.song ) {
+      console.log('path: ' + this.file.applicationDirectory);
+      let path = (this.file.applicationDirectory + 'www/assets/media/LeanOnMe.mp3' ).replace(/^file:\/\//, '');
+      this.song = this.media.create( path );
+      // this.song = this.media.create( "../../assets/media/LeanOnMe.mp3" );
+      this.song.play();
+    }
+      // if song is paused, don't recreate it; just play it
+    else if ( this.isSongPaused ) {
+      this.song.play();
+      this.isSongPaused = false;
+    }
+  }
+  catch ( e ) {
+    console.log( 'error playing song\n' + e );
+  }
+}
+
+public pauseMusic() {
+  // only try to pause if the song exists
+  if ( this.song ) {
+    this.song.pause();
+    this.isSongPaused = true;
+  }
+}
+
+public stopMusic() {
+  // stop the song, only if it exists
+  if ( this.song ) {
+    this.song.stop();
+    this.song.release(); // frees up the memory
+    this.song = null;
+    this.isSongPaused = false;
+  }
+}
+
+
 
 }
